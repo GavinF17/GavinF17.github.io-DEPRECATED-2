b4f167c7af60a19c7a2da042c6ca4217cc89081f
Gavin Fenton <20977010+GavinF17@users.noreply.github.com>
2020-08-22 21:12:38 +0100

    Added navigation with hamburger

---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import Navbar from "reactstrap/es/Navbar";
+import {NavbarToggler} from "reactstrap";
+import Collapse from "reactstrap/es/Collapse";
+import Nav from "reactstrap/es/Nav";
+import NavItem from "reactstrap/es/NavItem";
+import NavLink from "reactstrap/es/NavLink";
+
+export const Navigation = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleNavbar = () => setOpen(!open);
+
+  return (
+    <Navbar expand="md" className="navigation">
+      <NavbarToggler onClick={toggleNavbar} >
+        <button className={"hamburger hamburger--collapse" + (open ? " is-active" : "")} type="button">
+          <span className="hamburger-box">
+            <span className="hamburger-inner"/>
+          </span>
+        </button>
+      </NavbarToggler>
+
+
+      <Collapse isOpen={open} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/">Gavin Fenton</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/experience">Experience</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/blog">Blog</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
