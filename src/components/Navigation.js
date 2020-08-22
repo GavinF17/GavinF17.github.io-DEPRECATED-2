e846564a8b7127ac8af11b6c1224e03ab947b44b
Gavin Fenton <20977010+GavinF17@users.noreply.github.com>
2020-08-22 21:26:14 +0100

    Changed nav linked to use router <Links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ import Collapse from "reactstrap/es/Collapse";
 import Nav from "reactstrap/es/Nav";
 import NavItem from "reactstrap/es/NavItem";
 import NavLink from "reactstrap/es/NavLink";
+import {Link} from "react-router-dom";
 
 export const Navigation = () => {
   const [open, setOpen] = useState(false);
@@ -13,7 +14,7 @@ export const Navigation = () => {
 
   return (
     <Navbar expand="md" className={"navigation" + (open ? " open" : "")}>
-      <NavbarToggler onClick={toggleNavbar} >
+      <NavbarToggler onClick={toggleNavbar}>
         <button className={"hamburger hamburger--collapse" + (open ? " is-active" : "")} type="button">
           <span className="hamburger-box">
             <span className="hamburger-inner"/>
@@ -25,13 +26,19 @@ export const Navigation = () => {
       <Collapse isOpen={open} navbar>
         <Nav className="mr-auto" navbar>
           <NavItem>
-            <NavLink href="/">Gavin Fenton</NavLink>
+            <Link to={"/"}>
+              <NavLink>Gavin Fenton</NavLink>
+            </Link>
           </NavItem>
           <NavItem>
-            <NavLink href="/experience">Experience</NavLink>
+            <Link to={"/experience"}>
+              <NavLink>Experience</NavLink>
+            </Link>
           </NavItem>
           <NavItem>
-            <NavLink href="/blog">Blog</NavLink>
+            <Link to={"/blog"}>
+              <NavLink>Blog</NavLink>
+            </Link>
           </NavItem>
         </Nav>
       </Collapse>
